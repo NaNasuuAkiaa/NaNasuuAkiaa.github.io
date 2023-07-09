1f7be17beab7c9024c80cb6a074d0750226baadc
NaNasuuAkiaa <brunoguerrah@gmail.com>
2023-07-09 10:14:57 +0200

    rainbowdone

--- script.js
+++
@@ -1,9 +0,0 @@
-const gravityText = document.querySelector(".gravity-text");
-
-gravityText.addEventListener("mouseover", function() {
-  this.style.animationPlayState = "running"; /* Inicia la animación al pasar el cursor sobre el elemento */
-});
-
-gravityText.addEventListener("mouseout", function() {
-  this.style.animationPlayState = "paused"; /* Pausa la animación al sacar el cursor del elemento */
-});
